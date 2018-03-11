c6974cb62398e18782899eb5bf807620272a8d83
Marek Stasikowski <marek.stasikowski@me.com>
2018-03-11 19:36:23 +0000

    add hero banner with correct title

--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
 import React from 'react'
-import {render} from 'react-dom'
+import { render } from 'react-dom'
 
 const appContainer = document.querySelector('body')
-const app = <div className='hero'>
-  <div className='container'>
-    <h1>Hello!</h1>
-  </div>
-</div>
+const app =
+  <section className='hero is-success is-bold'>
+    <div className='hero-body'>
+      <div className='container'>
+        <h1 className='title'>QMetric Shop</h1>
+      </div>
+    </div>
+  </section>
 
 render(app, appContainer)
