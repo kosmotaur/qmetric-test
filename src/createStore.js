bc27a3da7f625aee1b5f4b3f08ff52b52b9f5dbc
Marek Stasikowski <marek.stasikowski@me.com>
2018-03-13 07:48:09 +0000

    add support for Redux dev tool

--- src/createStore.js
+++ src/createStore.js
@@ -1,9 +1,10 @@
-import {createStore, combineReducers} from 'redux'
+import { combineReducers, createStore } from 'redux'
 import productsReducer from './products/reducer'
 
 export default initialState => createStore(
   combineReducers({
     products: productsReducer
   }),
-  initialState
+  initialState,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 )
